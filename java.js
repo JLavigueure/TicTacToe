315456440453b4f324767f396d049305b5a81fea
Jordan <lav.jordan@live.com>
2022-07-28 02:07:52 -0700

    AI functions complete

--- java.js
+++ java.js
@@ -1,12 +1,17 @@
-// create object for gameboard
-    // set array for game board
-    // method for printing gameboard to screen.. render
-    // make each block a div
-    // method for adding listener evnts to all divs
-    // method for adding input to array
 // create factory function for players
-    // variable to track score for best out of 3
+    // name?
+    // variable to track score
     // variable to track players icon
+
+// when CPU is selected, add off class to .playersInput #2
+// save names and state winner is (insert name)
+    // if no name present, state X or O
+
+
+// TO DO 7/28
+// Add AI turns to on click, use if statement to check if on 2 player or not first
+// add AI function to check for wins first, not just any two cells in a line filled with same symbols
+// fix bug: 'AI error. No move made' shows in console sometimes, AI still appears to be working though
 
 let gameboard = {
     board: new Array(9), 
@@ -14,6 +19,9 @@ let gameboard = {
     turn: '',
     modalElements: [],
     DOM: [],
+    buttons: [],
+    twoPlayer: false,
+    nameInputs: [],
 
     cacheDOM: function() { 
         let cache = document.querySelectorAll('.cell');
@@ -22,6 +30,12 @@ let gameboard = {
     cacheModal: function() {
         modalElements = Array.from(document.querySelectorAll(".modalClass"));
         return modalElements;
+    },
+    cacheButtons: function() {
+        return(Array.from(document.querySelectorAll('.mode')))
+    },
+    cacheNames: function() {
+        return(Array.from(document.querySelectorAll('.playersInput')))
     },
     openModal: function() {
         for(let i = 0; i < this.modalElements.length; i++){
@@ -32,6 +46,25 @@ let gameboard = {
         gameboard.modalElements.forEach(element => {
             element.classList.remove('show');
         });
+    },
+    change2Player: function() {
+        // adjust HTML classes for css changes
+        let selectedBtnClassList = Array.from(event.srcElement.classList);
+        if(selectedBtnClassList.includes('selected')) {
+            return 1;
+        }
+        for(let i = 0; i < this.buttons.length; i++) {
+            this.buttons[i].classList.remove('selected');
+        }
+        event.srcElement.classList.add('selected');
+        // change javascript variable
+        if(event.srcElement.innerHTML == 'CPU') {
+            gameboard.twoPlayer = false;
+            gameboard.nameInputs[1].classList.add('off'); }
+        else { 
+            gameboard.twoPlayer = true;
+            gameboard.nameInputs[1].classList.remove('off'); 
+        }
     },
 
     render: function() { 
@@ -53,8 +86,8 @@ let gameboard = {
         }
     }, 
     onClick: function(){
+        // gameboard listeners
         for(let i = 0; i < this.boardLength; i++){
-        // add event listeners
             gameboard.DOM[i].addEventListener('click', function() {
                 if(gameboard.board[i]) {return};
                 if(gameboard.turn == 'x') {
@@ -64,54 +97,58 @@ let gameboard = {
                     gameboard.board[i] = 'o';
                     gameboard.turn = 'x';
                 }
-                console.log(gameboard.turn);
-                console.log('player changed!')
                 gameboard.resetDOM();
                 gameboard.render();
                 gameboard.checkWinner();
             });
         }
-        this.modalElements[1].addEventListener('click', function() {
+        // modal listeners
+        this.modalElements[2].addEventListener('click', function() {
             gameboard.newGame();
         })
+        // button listeners
+        for(let i = 0; i < 2; i++) {
+            this.buttons[i].addEventListener('click', function() {
+                gameboard.change2Player();
+            })
+        }    
     },
     checkWinner: function() {
         // check 3x verticals
         for(let i = 0; i < 4; i++){
             if(gameboard.board[i] == gameboard.board[(i+3)] && gameboard.board[i] == gameboard.board[(i+6)] && gameboard.board[i]) {
-                console.log(`Winner is ${gameboard.board[i]}`)
                 gameboard.openModal();
+                gameboard.modalElements[1].innerHTML = `The winner is ${gameboard.board[i]}`;
                 return gameboard.board[i];
             } 
         }
         //check 3x horizontals
         for(let i = 0; i <= 6; i += 3){
             if(gameboard.board[i] == gameboard.board[(i+1)] && gameboard.board[i] == gameboard.board[(i+2)] && gameboard.board[i]){
-                console.log(`Winner is ${gameboard.board[i]}`)
                 gameboard.openModal();
+                gameboard.modalElements[1].innerHTML = `The winner is ${gameboard.board[i]}`;
                 return gameboard.board[i];
             }
         }
         //check 2x diagnols
         if(gameboard.board[0] == gameboard.board[4] && gameboard.board[0] == gameboard.board[8] && gameboard.board[0]){
-            console.log(`Winner is ${gameboard.board[0]}`)
             gameboard.openModal();
+            gameboard.modalElements[1].innerHTML = `The winner is ${gameboard.board[0]}`;
             return gameboard.board[0];
         }
         if(gameboard.board[2] == gameboard.board[4] && gameboard.board[2] == gameboard.board[6] && gameboard.board[2]){
-            console.log(`Winner is ${gameboard.board[2]}`)
             gameboard.openModal();
+            gameboard.modalElements[1].innerHTML = `The winner is ${gameboard.board[2]}`;
             return gameboard.board[2];
         }
-        //check board array is full 
+        //check board array is not full 
         for(let i = 0; i < gameboard.board.length; i++){
             if(gameboard.board[i] == false) {
-                console.log('keep playing!')
                 return;
             }
         }
-        console.log(`its a draw!`);
         gameboard.openModal();
+        gameboard.modalElements[1].innerHTML = `It's a draw!`;
         return 'draw';
     },
     resetDOM: function() {
@@ -124,7 +161,9 @@ let gameboard = {
     init: function() {
         // cache & reset DOM
         this.modalElements = gameboard.cacheModal();
-        gameboard.DOM = gameboard.cacheDOM();
+        this.buttons = gameboard.cacheButtons();
+        this.DOM = gameboard.cacheDOM();
+        this.nameInputs = gameboard.cacheNames();
         // add listener events
         this.onClick();
         //reset game
@@ -140,6 +179,170 @@ let gameboard = {
         gameboard.turn = 'x';
         gameboard.closeModal();
     },
+}
+
+let AI = {
+    firstMove: function() {
+        // if center is played
+        if(gameboard.board[4] == 'x'){
+            switch(this.randomInt(4)) {
+                case 0: gameboard.DOM[0].click(); break;
+                case 1: gameboard.DOM[2].click(); break;
+                case 2: gameboard.DOM[6].click(); break;
+                case 3: gameboard.DOM[8].click(); break;
+            }
+            return;
+        }
+        // if middles are playerd
+        for(let i = 0; i < 4; i++) {
+            if(gameboard.board[(2 * i) + 1] == 'x') {
+                gameboard.DOM[4].click();
+                return;
+            }
+        }
+        // if corners are played
+        if(gameboard.board[0] == 'x') {
+            switch(this.randomInt(2)){
+                case 0: gameboard.DOM[1].click(); break;
+                case 1: gameboard.DOM[3].click(); break;
+            }
+            return;
+        }
+        if(gameboard.board[2] == 'x') {
+            switch(this.randomInt(2)){
+                case 0: gameboard.DOM[1].click(); break;
+                case 1: gameboard.DOM[5].click(); break;
+            }
+            return;
+        }
+        if(gameboard.board[6] == 'x') {
+            switch(this.randomInt(2)){
+                case 0: gameboard.DOM[3].click(); break;
+                case 1: gameboard.DOM[7].click(); break;
+            }
+            return;
+        }
+        if(gameboard.board[8] == 'x') {
+            switch(this.randomInt(2)){
+                case 0: gameboard.DOM[5].click(); break;
+                case 1: gameboard.DOM[7].click(); break;
+            }
+            return;
+        }
+
+    },
+    checkWinRows: function() {
+        let centerCell;
+        // for each row
+        for(let i = 0; i < 3; i++) {
+            centerCell = (3 * i) + 1;
+            // check to see if two of three cells are filled
+            let cellsFilled = 0;
+            for(let j = -1; j < 2; j++) {
+                if(gameboard.board[centerCell + j]) {
+                    cellsFilled++;
+                }
+            }
+            if(cellsFilled != 2){
+                continue;
+            }
+            // check if two filled cells are the same, if so click cells
+            if(gameboard.board[centerCell] == gameboard.board[centerCell - 1] ||
+                gameboard.board[centerCell] == gameboard.board[centerCell + 1] ||
+                gameboard.board[centerCell - 1] == gameboard.board[centerCell + 1]) {
+                    for(let j = -1; j < 2; j++) {
+                        gameboard.DOM[centerCell + j].click();
+                    }
+                    return 1;
+                }
+        }
+    },
+    checkWinColumns: function() {
+        let centerCell;
+        // for each column
+        for(let i = 0; i < 3; i++){
+            centerCell = (i + 3);
+            // check to see if two of three cells are filled
+            let cellsFilled = 0;
+            for(let j = -3; j <= 3; j += 3){
+                if(gameboard.board[centerCell + j]) {
+                    cellsFilled++;
+                }
+            }
+            if(cellsFilled != 2){
+                continue;
+            }
+            // compare cells
+            if(gameboard.board[centerCell] == gameboard.board[centerCell - 3] ||
+                gameboard.board[centerCell] == gameboard.board[centerCell + 3] ||
+                gameboard.board[centerCell - 3] == gameboard.board[centerCell + 3]) {
+                    for(let j = -3; j <= 3; j += 3) {
+                        gameboard.DOM[centerCell + j].click();
+                    }
+                    return 1;
+                }
+        }
+    },
+    checkWinDiagnols: function() {
+        // check to see if two of three cells are filled for either diagnol
+        let cellsFilledLeft = 0;
+        let cellsFilledRight = 0;
+        for(let i = 0; i <= 4; i += 2){
+            if(gameboard.board[i * 2]){cellsFilledLeft++}
+            if(gameboard.board[(i + 2)]){cellsFilledRight++}
+        }
+        // compare left cells
+        if(cellsFilledLeft == 2) {
+            if(gameboard.board[0] == gameboard.board[4] ||
+                gameboard.board[4] == gameboard.board[8] ||
+                gameboard.board[0] == gameboard.board[8]) {
+                    for(let i = 0; i <= 4; i += 2){
+                        gameboard.DOM[i * 2].click();
+                    }
+                    return 1;
+                }
+        }
+        // compare right cells
+        if(cellsFilledRight == 2) {
+            if(gameboard.board[2] == gameboard.board[4] ||
+                gameboard.board[4] == gameboard.board[6] ||
+                gameboard.board[2] == gameboard.board[6]) {
+                    for(let i = 0; i <= 4; i += 2){
+                        gameboard.DOM[i + 2].click();
+                    }
+                    return 1;
+            }
+        }
+    },
+    randomMove: function() {
+        let randomCell = this.randomInt(9);
+        if(gameboard.board[randomCell] == ''){
+            gameboard.DOM[randomCell].click();
+            return 1;
+        }
+        else{this.randomMove()}
+    },
+    randomInt: function(max) {
+        return Math.floor(Math.random() *  max);
+    },
+    takeTurn: function(){
+        // check to see if first move of game
+        let cellsFilled = 0;
+        for(let i = 0; i < gameboard.board.length; i++){
+            if(gameboard.board[i]){cellsFilled++}
+            if(cellsFilled > 1){break}
+        }
+        if(cellsFilled == 1){
+            console.log('first move made');
+            AI.firstMove();
+            return;
+        }
+        if(this.checkWinRows() == 1){console.log('Row'); return;}
+        if(this.checkWinColumns() == 1){console.log('Column'); return;}
+        if(this.checkWinDiagnols() == 1){console.log('Diagnol'); return;}
+        if(this.randomMove() == 1){console.log('Random'); return;}
+        console.log('AI Error. No move made');
+    }
 }
 
 gameboard.init();
